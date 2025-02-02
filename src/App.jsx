01aa801702f0fbc1d0dca5c0eb4c5350e15a58e5
akash-panthri <akashpanthri2000@gmail.com>
2025-02-02 06:22:50 +0530

    Added logo in Header.jsx in sections folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
 import "./App.css";
+import Header from "./sections/Header";
 export default function App() {
   return (
     <main className="overflow-hidden">
-      
+      <Header />
     </main>
   );
 }
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,7 +1,11 @@
-
-
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="fixed top-0 left-0 z-50 w-full py-10">
+      <div className="container flex h-14 items-center max-lg:px-5">
+        <a href="" className="lg:hidden flex-1 cursor-pointer z-2">
+          <img src="/images/hebbian.svg" width={115} height={55} alt="logo" />
+        </a>
+      </div>
+    </header>
+  );
 }
