401bfb304e75d36c3b8c61aa3d2f0071581191dd
akash-panthri <akashpanthri2000@gmail.com>
2025-02-03 06:04:27 +0530

    Added LinkScroll in Header.jsx in sections folder

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,7 +1,10 @@
 import { Link as LinkScroll } from "react-scroll";
 
 const NavLink = ({ title }) => (
-<LinkScroll>{title}</LinkScroll>);
+  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-1000 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
+    {title}
+  </LinkScroll>
+);
 
 export default function Header() {
   return (
@@ -10,16 +13,33 @@ export default function Header() {
         <a href="" className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/hebbian.svg" width={140} height={55} alt="logo" />
         </a>
-        <div className="w-full border-2 border-amber-400">
-          <nav>
+        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+        <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+          <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
             <ul className="flex max-lg:block max-lg:px-12">
               <li className="nav-li">
                 <NavLink title="features" />
-                <div className="dot"/>
+                <div className="dot" />
                 <NavLink title="pricing" />
               </li>{" "}
+              <li className="nav-logo">
+                <LinkScroll>
+                  <img
+                    src="/images/hebbian.svg"
+                    width={145}
+                    height={55}
+                    alt="logo"
+                  />
+                </LinkScroll>
+              </li>
+              <li className="nav-li">
+                <NavLink title="faq" />
+                <div className="dot" />
+                <NavLink title="download" />
+              </li>
             </ul>
           </nav>
+        </div>
         </div>
       </div>
     </header>
