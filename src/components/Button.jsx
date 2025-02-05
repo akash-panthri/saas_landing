a1af7d98722a5c47454ff3771bc80c7d2e864e0a
akash-panthri <akashpanthri2000@gmail.com>
2025-02-05 06:41:52 +0530

    Added alternative button element code in Button.jsx in components folder

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,21 @@
 import clsx from "clsx";
 
-export default function Button({ icon, children }) {
-  return (
+export default function Button({ icon, children, href, containerClassName }) {
+  return href ? (
+    <a
+      className={clsx(
+        "relative p-0.5 g5 rounded-2xl shadow-500 group",
+        containerClassName
+      )}
+    ></a>
+  ) : (
     <button
-      className={clsx("relative p-0.5 g5 rounded-2xl shadow-500 group")}
-    > Button </button>
+      className={clsx(
+        "relative p-0.5 g5 rounded-2xl shadow-500 group",
+        containerClassName
+      )}
+    >
+      Button
+    </button>
   );
 }
