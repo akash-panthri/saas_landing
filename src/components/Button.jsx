2f3bde83edc6cd020d16120eebff7b603b29d424
akash-panthri <akashpanthri2000@gmail.com>
2025-02-06 06:04:31 +0530

    Added Inner element in Button.jsx in components folder

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,42 @@
 import clsx from "clsx";
 
-export default function Button({ icon, children, href, containerClassName }) {
+export default function Button({ icon, children, href, containerClassName,onClick }) {
+    const Inner = () => (
+        <>
+        <span>
+            <span>Marker</span>
+            {icon && (
+          <img
+            src={icon}
+            alt="circle"
+            className="size-10 mr-5 object-contain z-10"
+          />
+        )}
+        <span className="relative z-2 font-poppins base-bold text-p1 uppercase">
+          {children}
+        </span>
+        </span>
+        
+        </>)
   return href ? (
     <a
       className={clsx(
         "relative p-0.5 g5 rounded-2xl shadow-500 group",
         containerClassName
       )}
-    ></a>
+      href={href}
+    >
+        <Inner />
+    </a>
   ) : (
     <button
       className={clsx(
         "relative p-0.5 g5 rounded-2xl shadow-500 group",
         containerClassName
       )}
+      onClick={onClick}
     >
-      Button
+      <Inner />
     </button>
   );
 }
