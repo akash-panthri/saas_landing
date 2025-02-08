fa7547a0f45c59556e6be4eeb35197833e0d2432
akash-panthri <akashpanthri2000@gmail.com>
2025-02-08 06:05:46 +0530

    Added basic code to Features.jsx in sections folder

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,6 +1,13 @@
+import { Element } from "react-scroll";
 
 export default function Features() {
   return (
-    <div>Features</div>
-  )
+    <section>
+      <Element name="features">
+        <div className="container">
+          <div className="relative flex"></div>
+        </div>
+      </Element>
+    </section>
+  );
 }
