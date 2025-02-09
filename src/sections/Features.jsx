26ec3e31ba6034324af548be72c8236f873ce013
akash-panthri <akashpanthri2000@gmail.com>
2025-02-09 06:44:03 +0530

    Added feature text and button in features.jsx

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,5 +1,6 @@
 import { Element } from "react-scroll";
 import { features } from "../components/constants";
+import Button from "../components/Button";
 
 export default function Features() {
   return (
@@ -24,6 +25,12 @@ export default function Features() {
                     />
                   </div>
                 </div>
+                <p className="caption mb-5 max-md:mb-6">{caption}</p>
+                <h2 className="max-w-400 mb-7 h3 text-p4 max-md:mb-6 max-md:h5">
+                  {title}
+                </h2>
+                <p className="mb-11 body-1 max-md:mb-8 max-md:body-3">{text}</p>
+                <Button icon={button.icon}>{button.title}</Button>
               </div>
             ))}
           </div>
