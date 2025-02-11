59b42c1a5550cdd5533f70c6e2d747541cf242ce
akash-panthri <akashpanthri2000@gmail.com>
2025-02-11 06:28:16 +0530

    Added useEffect in Header.jsx in sections folder

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,5 +1,5 @@
-import clsx from "clsx";
-import { useState } from "react";
+import clsx from "clsx";;
+import { useState,useEffect } from "react";
 import { Link as LinkScroll } from "react-scroll";
 
 const NavLink = ({ title }) => (
@@ -10,8 +10,21 @@ const NavLink = ({ title }) => (
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  
+  const [hasScrolled, setHasScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header className={clsx("fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4", hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]",)}>
       <div className="container flex h-14 items-center max-lg:px-5">
         <a href="" className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/hebbian.svg" width={140} height={55} alt="logo" />
