c328284a9812f82f00eb00d6044443b37ff9abc9
akash-panthri <akashpanthri2000@gmail.com>
2025-02-11 06:07:46 +0530

    Added title in features.jsx

--- src/components/constants/index.js
+++ src/components/constants/index.js
@@ -4,7 +4,7 @@ export const features = [
       icon: '/images/feature-1.png',
       caption: 'Easy integration',
       title: 'Work smarter not harder',
-      text: "With Xora, tedious tasks are history. Automation and smart processes bring your productivity to new heights. It's like having an extra cup of coffee, but without the jitters.",
+      text: "With Hebbian, tedious tasks are history. Automation and smart processes bring your productivity to new heights. It's like having an extra cup of coffee, but without the jitters.",
       button: {
         icon: '/images/magictouch.svg',
         title: 'Watch the demo',
@@ -15,7 +15,7 @@ export const features = [
       icon: '/images/feature-2.png',
       caption: 'Secure & trustworthy',
       title: 'Sleep easy, we got your back',
-      text: 'Your data security is our priority. With state-of-the-art encryption and robust privacy controls, Xora helps keeps your information secure and locked up tighter than Fort Knox.',
+      text: 'Your data security is our priority. With state-of-the-art encryption and robust privacy controls, Hebbian keeps your information secure and locked up tighter than Fort Knox.',
       button: {
         icon: '/images/docs.svg',
         title: 'Read the docs',
@@ -49,13 +49,13 @@ export const features = [
   export const faq = [
     {
       id: '0',
-      question: 'How easy is it to setup Xora?',
+      question: 'How easy is it to setup Hebbian?',
       answer:
         'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
     },
     {
       id: '1',
-      question: 'Can I integrate Xora with other platforms?',
+      question: 'Can I integrate Hebbian with other platforms?',
       answer:
         'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
     },
