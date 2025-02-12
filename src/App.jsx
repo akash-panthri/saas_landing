7a85b7500d4cac65af10ef32b7e1ae03fc545010
akash-panthri <akashpanthri2000@gmail.com>
2025-02-12 06:07:31 +0530

    Added Pricing.jsx in sections folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Features from "./sections/Features";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
+import Pricing from "./sections/Pricing";
 export default function App() {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
       <Features />
+      <Pricing />
     </main>
   );
 }
