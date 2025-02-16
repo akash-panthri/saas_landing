a8b2f29ed1e04c3587b87c3afc635a5967ab713a
akash-panthri <akashpanthri2000@gmail.com>
2025-02-16 06:41:34 +0530

    Added faq logo to Faq.jsx

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -3,13 +3,25 @@ import { Element } from "react-scroll";
 export default function Faq() {
   return (
     <section>
-    <Element name="faq" className="relative"></Element>
-    <div className=""><h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
-              Curiosity didn&apos;t kill the cat, it gave it answers.
-            </h3>
-            <p className="body-1 max-lg:max-w-sm">
-              You&apos;ve got questions, we&apos;ve got answers.
-            </p></div>
+      <Element name="faq" className="relative"></Element>
+      <div className="container relative z-2 py-28">
+        <div>
+          <h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
+            Curiosity didn&apos;t kill the cat, it gave it answers.
+          </h3>
+          <p className="body-1 max-lg:max-w-sm">
+            You&apos;ve got questions, we&apos;ve got answers.
+          </p>
+        </div>
+        <div className="faq-line_after w-0.5 h-full absolute left-[calc(50%-1px)] top-0 -z-1 bg-s2" />
+      </div>
+      <div className="faq-glow_before relative z-2 border-2 border-s2 bg-s1">
+        <div className="container flex gap-10 max-lg:block">
+          <div className="rounded-half absolute -top-10 left-[calc(50%-40px)] z-4 flex size-20 items-center justify-center border-2 border-s2 bg-s1">
+            <img src="/images/faq-logo.svg" alt="logo" className="size-1/2" />
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
