1a499f53417a6f9e5b64ee6a3e75fd170f0dc196
akash-panthri <akashpanthri2000@gmail.com>
2025-02-16 06:23:52 +0530

    Added basic code to Faq.jsx

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,6 +1,15 @@
+import { Element } from "react-scroll";
 
 export default function Faq() {
   return (
-    <div>Faq</div>
+    <section>
+    <Element name="faq" className="relative"></Element>
+    <div className=""><h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
+              Curiosity didn&apos;t kill the cat, it gave it answers.
+            </h3>
+            <p className="body-1 max-lg:max-w-sm">
+              You&apos;ve got questions, we&apos;ve got answers.
+            </p></div>
+    </section>
   )
 }
