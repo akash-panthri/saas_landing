7207434f36a0532d23857f4d8bdefa9280b32a86
akash-panthri <akashpanthri2000@gmail.com>
2025-02-18 07:16:36 +0530

    Finalized Faq.jsx and FaqItem.jsx

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -6,7 +6,8 @@ export default function Faq() {
     const halfLength = Math.floor(faq.length / 2);
   return (
     <section>
-      <Element name="faq" className="relative"></Element>
+      <Element name="faq" className="relative">
+
       <div className="container relative z-2 py-28">
         <div>
           <h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
@@ -28,8 +29,16 @@ export default function Faq() {
                 <FaqItem key={item.id} item={item} index={index} />
               ))}
             </div>
+            <div className="relative flex-1 lg:pt-24">
+              {faq.slice(halfLength).map((item, index) => (
+                <FaqItem key={item.id} item={item} index={halfLength + index} />
+              ))}
+            </div>
         </div>
+      <div className="faq-lin_after absolute left-[calc(50%-1px)] top-0 -z-1 h-full w-0.5 bg-s2 max-lg:hidden" />
       </div>
+        
+      </Element>
     </section>
   );
 }
