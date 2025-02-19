049d87835aa9c6fd6d2371463d13d0299e1abe5a
akash-panthri <akashpanthri2000@gmail.com>
2025-02-19 06:03:11 +0530

    Added Testimonials.jsx in sections folder

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Features from "./sections/Features";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Pricing from "./sections/Pricing";
+import Testimonials from "./sections/Testimonials";
 export default function App() {
   return (
     <main className="overflow-hidden">
@@ -12,6 +13,7 @@ export default function App() {
       <Features />
       <Pricing />
       <Faq />
+      <Testimonials />
     </main>
   );
 }
