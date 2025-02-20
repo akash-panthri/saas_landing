6bfa5f276a83be9fa43c7befff4d2497fe70fcee
akash-panthri <akashpanthri2000@gmail.com>
2025-02-20 06:08:57 +0530

    Added TestimonialItem.jsx in components folder

---
+++ src/sections/TestimonialItem.jsx
@@ -0,0 +1,6 @@
+
+export default function TestimonialItem() {
+  return (
+    <div>TestimonialItem</div>
+  )
+}
--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,10 +1,20 @@
+import { testimonials } from "../constants";
+
 export default function Testimonials() {
+  const halfLength = Math.floor(testimonials.length / 2);
   return (
     <section className="relative z-2 py-24 md:py-28 lg:py-40">
       <div className="container block lg:flex">
-      <div className="testimonials_head-res relative z-2 mr-20 flex-300">
+        <div className="testimonials_head-res relative z-2 mr-20 flex-300">
           <p className="caption mb-5 max-md:mb-2.5">Wall of Love</p>
           <h3 className="h3 max-md:h5 text-p4">Words from our fans</h3>
+        </div>
+        <div className="testimonials_inner-after testimonials_inner-before relative -my-12 -mr-3 flex items-start max-lg:static max-md:block">
+          <div className="testimonials_group-after flex-50">
+            {testimonials.slice(0, halfLength).map((testimonial) => (
+              <div key={testimonial.id}>{testimonial.name}</div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
